675e864220f82f23433b770fa5f92b5082addda4
wntl <1536183348@qq.com>
2019-08-09 20:14:58 +0800

    初始化版本

---
+++ newspro/src/main/java/org/mine/controller/Hello/HelloWorldController.java
@@ -0,0 +1,27 @@
+package org.mine.controller.Hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/helloWorld")
+public class HelloWorldController {
+
+	@RequestMapping("/view.do")
+	public String showViews(){
+		return "show";
+	}
+	
+	@RequestMapping("/view1.do")
+	public ModelAndView showViews_1(){
+		return new ModelAndView("show1");
+		
+	}
+	
+	@RequestMapping("/htmlview.do")
+	public String showHtmlViews(){
+		return "htmlView";
+	}
+	
+}
